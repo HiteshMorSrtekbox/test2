56f43d9e7a9c0bc62b6da01a2269fb846dd637de
srtekMAC <srtekmac@srtekmacs-macbook-air.local>
2018-11-27 10:09:41 +0530

    Initial Commit

---
+++ test2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test2
+//
+//  Created by srtekMAC on 27/11/18.
+//  Copyright © 2018 srtekMAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
